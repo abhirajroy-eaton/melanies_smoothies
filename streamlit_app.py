a7d55ed301d269ae8f63e4c853b7b3ad1ef01deb
abhirajroy-eaton <112368162+abhirajroy-eaton@users.noreply.github.com>
2024-06-16 01:35:12 +0530

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -9,13 +9,19 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
+import streamlit as st
 
-session = get_active_session()
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name of your Smoothie will be:", name_on_order)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect('Choose up to 5 ingredeients:',my_dataframe)
+ingredients_list = st.multiselect('Choose up to 5 ingredeients:'
+                                  ,my_dataframe
+                                  ,max_selections=5)
 
 if ingredients_list:
  #   st.write(ingredients_list)
@@ -28,10 +34,13 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('"""+ ingredients_string +"""','"""+name_on_order+"""')"""
+
+
 
     #st.write(my_insert_stmt)
+    #st.stop()
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
